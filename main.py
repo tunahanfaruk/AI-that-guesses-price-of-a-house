36913960099dfa2207de55a1dc8cfca714667974
tunahanfaruk <trptooxd@proton.me>
2025-03-13 22:39:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+dataset = 'Housing.csv'
+df = pd.read_csv(dataset)
+
+X = df[['area', 'parking']] 
+y = df['price']            
+
+reg = LinearRegression()
+reg.fit(X, y)
+
+w = float(input("Area of a House: "))
+q = float(input("Number of House Parkings: "))
+
+guess_data = pd.DataFrame([[w, q]], columns=['area', 'parking'])
+
+guess = reg.predict(guess_data)
+print("Guessed price of the Houses:", guess[0])
